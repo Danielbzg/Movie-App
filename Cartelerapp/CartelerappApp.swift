c7b03d760a0c5e9daee6a5d925b13b616f1e5f19
Daniel BG <daniel.bozagarciagmail.com>
2023-03-29 12:12:23 +0200

    Primera subida

---
+++ Cartelerapp/CartelerappApp.swift
@@ -0,0 +1,17 @@
+//
+//  CartelerappApp.swift
+//  Cartelerapp
+//
+//  Created by alp1 on 29/3/23.
+//
+
+import SwiftUI
+
+@main
+struct CartelerappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cartelerapp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Cartelerapp
+//
+//  Created by alp1 on 29/3/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
