63c4e00cad41ca786643b3ffa132c0cf562d1fc5
Álvaro Murillo <alvaro.murillo@mobivery.com>
2023-04-10 10:28:07 +0200

    Refactor api call to repository

--- Cartelerapp/Repository/Repository.swift
+++ Cartelerapp/Repository/Repository.swift
@@ -7,3 +7,51 @@
 
 import Foundation
 import SwiftUI
+
+enum MoviesAPI {
+
+    enum Domain: String {
+        case production = "https://api.themoviedb.org"
+    }
+
+    enum APIKey: String {
+        case production = "1c8d728618e95027b688d25724ea8fbb"
+    }
+
+    enum Endpoint: String {
+        case movies = "/3/movie/now_playing"
+    }
+}
+
+
+class Repository {
+
+    private let decoder: JSONDecoder = {
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        return decoder
+    }()
+
+    let domain: MoviesAPI.Domain
+    let apiKey: MoviesAPI.APIKey
+
+    init(domain: MoviesAPI.Domain = .production, apiKey: MoviesAPI.APIKey = .production) {
+        self.domain = domain
+        self.apiKey = apiKey
+    }
+
+    private func url(_ endpoint: MoviesAPI.Endpoint) -> URL {
+        URL(string: domain.rawValue + endpoint.rawValue + "?api_key=\(apiKey.rawValue)&language=\(Locale.current.identifier)")!
+    }
+
+    func moviesInTheatres() async throws -> [Movie] {
+        let url: URL = url(.movies)
+        print(url)
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"        
+        let response = try await URLSession.shared.data(for: request)
+        let data: Data = response.0
+        let result = try decoder.decode(Response.self, from: data)
+        return result.results
+    }
+}
--- Cartelerapp/Views/Cartelera.swift
+++ Cartelerapp/Views/Cartelera.swift
@@ -49,7 +49,7 @@ struct Cartelera: View {
             }
             .frame(maxWidth: .infinity, maxHeight: 800, alignment: .center)
             .background(Color.indigo)
-            .onAppear(perform: moviesInTheatres)
+
             HStack {
                 //Botón 1
                 Button(action: {
@@ -85,37 +85,21 @@ struct Cartelera: View {
             .background(Color.mint)
         } //Fin VStack principal
         .background(LinearGradient(colors: [Color.orange, Color.red], startPoint: .top, endPoint: .center))
+        .onAppear { moviesInTheatres() }
     }
+
     
     func moviesInTheatres() {
-        guard let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=1c8d728618e95027b688d25724ea8fbb&language=es-ES!)") else {
-            return
-        }
-        
-        URLSession.shared.dataTask(with: url) { (data, response, error) in
-            if let error = error {
-                print(error.localizedDescription)
-                return
-            }
-            
-            guard let data = data else {
-                print("Data is nil")
-                return
-            }
-            
+
+        Task {
             do {
-                let decoder = JSONDecoder()
-                decoder.keyDecodingStrategy = .convertFromSnakeCase
-                
-                let response = try decoder.decode(Response.self, from: data)
-                
-                DispatchQueue.main.async {
-                    self.movies = response.results
-                }
-            } catch let error {
-                print(error.localizedDescription)
+                let movies = try await Repository().moviesInTheatres()
+                print(movies)
+                self.movies = movies
+            } catch {
+                print(error)
             }
-        }.resume()
+        }
     }
 }
 
