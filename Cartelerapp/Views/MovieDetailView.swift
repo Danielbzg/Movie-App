60e56f8d5e092b13557487da97f9989773d747de
Daniel BG <daniel.bozagarciagmail.com>
2023-04-19 18:01:19 +0200

    Modificaciones de diseño y organización de elementos. Aún por terminar.

--- Cartelerapp/Views/MovieDetailView.swift
+++ Cartelerapp/Views/MovieDetailView.swift
@@ -13,33 +13,79 @@ struct MovieDetailView: View {
     let movie: Movie
     @State var isFavorite: Bool
     @State var credits: String = ""
-
+    
     init(movie: Movie) {
         self.movie = movie
         self.isFavorite = movie.isFavorite
     }
-
+    
     var body: some View {
-
+        
         VStack(spacing: 0) {
             ScrollView(.vertical) {
-                VStack {
-                    
-                    Text(movie.title)
+                ZStack {
                     AsyncImage(url: RemoteImage.movieImage(path: movie.posterPath ?? "PosterDefault.jpg")) { image in image.resizable()
-                                            .aspectRatio(contentMode: .fit)
-                        } placeholder: {
-                            ProgressView()
+                            .aspectRatio(contentMode: .fit)
+                            .mask(LinearGradient(gradient: Gradient(colors: [Color.red, Color.red, Color.red, Color.blue.opacity(0)]), startPoint: .top, endPoint: .bottom))
+                    } placeholder: {
+                        ProgressView()
+                    } //Fin imagen
+                    
+                    VStack{
+                        Spacer()
+                        Text(movie.title)
+                            .foregroundColor(.white)
+                            .font(.custom("SF Pro Display", size: 28))
                     }
+                    
                 }
                 
                 HStack {
-                    Text("Estreno: ")
-                    
-                    Text(movie.releaseDate)
-                    
+                    HStack {
+                        Text("Estreno: ")
+                        
+                        Text(movie.releaseDate)
+                    }
                     Spacer()
+                    Text("·")
+                    Spacer()
+                    HStack {
+                        
+                        Text("Duración: ")
+                        Text(String(movie.voteAverage))
+                        
+                    }
+                    Spacer()
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.init(top: 1, leading: 5, bottom: 1, trailing: 5))
+                
+                //Géneros de la película
+                HStack {
+                    ZStack{
+                        Rectangle()
+                            .background(.blue)
+                            .cornerRadius(8)
+                    Text(String(movie.genreIds[0]))
+                            .padding(3)
+                            .foregroundColor(.gray)
+                        }
                     
+                    ZStack{
+                        Rectangle()
+                            .background(.blue)
+                            .cornerRadius(8)
+                    Text(String(movie.genreIds[1]))
+                            .padding(3)
+                            .foregroundColor(.gray)
+                        }
+                }
+                .frame(maxWidth: .infinity, alignment: .center)
+                .padding(.init(top: 1, leading: 5, bottom: 1, trailing: 5))
+                
+                
+                //Botones Favoritos - Pendientes...
+                HStack{
                     Button(action: {
                         if isFavorite {
                             Dependencies.repository.removeMovieFromFavourite(movie)
@@ -47,7 +93,7 @@ struct MovieDetailView: View {
                             Dependencies.repository.addMovieFavourite(movie)
                         }
                         isFavorite.toggle()
-
+                        
                         print("Película a añadir: \(movie)")
                     }, label: {
                         HStack{
@@ -56,7 +102,7 @@ struct MovieDetailView: View {
                                 .aspectRatio(contentMode: .fit)
                             
                             Text(isFavorite ? "Eliminar" : "Añadir")
-                    
+                            
                         } .padding([.leading, .trailing], 5)
                         
                     })
@@ -64,40 +110,23 @@ struct MovieDetailView: View {
                         .background(.gray)
                         .foregroundColor(.black)
                         .cornerRadius(8)
-                        
-                    Spacer()
-                    }
-                    .frame(maxWidth: .infinity, alignment: .leading)
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.init(top: 1, leading: 5, bottom: 1, trailing: 5))
+                
+                
+                
+                VStack{
+                    
+                    Text("Sinopsis")
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .font(.custom("Courier", fixedSize: 20))
+                    
+                    Text(movie.overview)
+                    
+                }.frame(maxWidth: .infinity, alignment: .leading)
                     .padding(.init(top: 1, leading: 5, bottom: 1, trailing: 5))
                 
-                HStack {
-                    
-                    Text("Valoración: ")
-                    Text(String(movie.voteAverage))
-                    
-                }
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .padding(.init(top: 1, leading: 5, bottom: 1, trailing: 5))
-                
-                HStack {
-                    
-                    Text("Género: ")
-                    Text(String(movie.genreIds[0]))
-                    
-                    Button(action: {
-                        print("Las películas favoritas son: \(Dependencies.repository.favouritesMovies())")
-                    }, label: {
-                        Text("Ver favoritas")
-                            .padding(3)
-                    })
-                        .frame(minWidth: 50)
-                        .background(.orange)
-                        .foregroundColor(.black)
-                        .cornerRadius(8)
-                }
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .padding(.init(top: 1, leading: 5, bottom: 1, trailing: 5))
-
                 if let director = movieCredits?.director {
                     HStack {
                         Text("Director: ")
@@ -108,23 +137,18 @@ struct MovieDetailView: View {
                 }
                 
                 VStack{
-                        
-                    Text("Sinopsis")
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                        .font(.custom("Courier", fixedSize: 20))
+                    Text("Reparto")
                     
-                    Text(movie.overview)
-                    
-                }.frame(maxWidth: .infinity, alignment: .leading)
-                .padding(.init(top: 1, leading: 5, bottom: 1, trailing: 5))
+                }
+                
             }
         }
         .background(LinearGradient(colors: [Color(red: 63/255, green: 132/255, blue: 229/255), Color(red: 24/255, green: 48/255, blue: 89/255)], startPoint: .top, endPoint: .center))
         .onAppear { loadCredits() }
     }
-
+    
     func loadCredits() {
-
+        
         Task {
             do {
                 let movieCredits = try await Dependencies.repository.moviesCredits(id: movie.id)
